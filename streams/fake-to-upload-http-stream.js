83bb436fe45acb7371b8450acddb135ca2689844
Alexandre Junior <alexandrejunior408@gmail.com>
2023-02-13 10:09:22 -0300

    feat: consumindo uma stream completa

---
+++ streams/fake-to-upload-http-stream.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { Readable } from "node:stream";
+
+class OneToHundredStream extends Readable {
+  index = 1;
+
+  _read() {
+    const i = this.index++;
+
+    setTimeout(() => {
+      if (i > 5) {
+        this.push(null);
+      } else {
+        const buf = Buffer.from(String(i));
+
+        this.push(buf);
+      }
+    }, 1000);
+  }
+}
+
+axios
+  .post("http://localhost:3334", new OneToHundredStream())
+  .then((response) => {
+    console.log(response.data);
+
+    return response.data;
+  });
