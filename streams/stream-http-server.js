7a43632e84583a0cdcdf2bac1e945bf39d54d956
Alexandre Junior <alexandrejunior408@gmail.com>
2023-02-13 22:41:48 -0300

    feat: cria corpo da requisição em JSON (Stream & Buffers)

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -12,16 +12,6 @@ class InverseNumberStream extends Transform {
 }
 
 const server = http.createServer(async (request, response) => {
-  const buffers = [];
-
-  for await (const chunk of request) {
-    buffers.push(chunk);
-  }
-
-  const fullStreamContent = Buffer.concat(buffers).toString();
-
-  console.log(fullStreamContent);
-
   return response.end(fullStreamContent);
 
   // return request.pipe(new InverseNumberStream()).pipe(response);
